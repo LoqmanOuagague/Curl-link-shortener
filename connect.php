3d6a2776595d26c66a505cc29ed86b28cf7b992e
Loqman Ouagague <loqman.ouagague@gmail.com>
2021-06-12 16:59:13 +0100

    Setting up the evironnement variable

--- connect.php
+++ connect.php
@@ -7,10 +7,6 @@ $env = Dotenv::createImmutable(__DIR__);
 $env->load();
 try {
     $pdo = new PDO($_ENV['DATABASE_DRIVER'] . ":host=" . $_ENV['DATABASE_HOST'] . ";port=" . $_ENV['DATABASE_PORT'] . ";dbname=" . $_ENV['DATABASE_NAME'] . ";user=" . $_ENV['DATABASE_USERNAME'] . ";password=" . $_ENV['DATABASE_PASSWORD']);
-
-    if ($pdo) {
-     echo'eee';
-    }
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
