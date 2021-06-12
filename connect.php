b5e01a849ead9dc49b010d87c4585b42134a75ff
Loqman Ouagague <loqman.ouagague@gmail.com>
2021-06-12 15:24:12 +0100

    Setting up  env

--- connect.php
+++ connect.php
@@ -6,10 +6,10 @@ use Dotenv\Dotenv;
 $env = Dotenv::createImmutable(__DIR__);
 $env->load();
 try {
-    $pdo = new PDO($_ENV['DATABASE_DRIVER'] . ":host=" . $_ENV['DATABASE_HOST'] . ";port=" . $_ENV['DATABASE_PORT'] . ";dbname=" . $_ENV['DATABASE_NAME'] . ";charset=UTF8;user=" . $_ENV['DATABASE_USERNAME'] . ";password=" . $_ENV['DATABASE_PASSWORD']);
+    $pdo = new PDO($_ENV['DATABASE_DRIVER'] . ":host=" . $_ENV['DATABASE_HOST'] . ";port=" . $_ENV['DATABASE_PORT'] . ";dbname=" . $_ENV['DATABASE_NAME'] . ";user=" . $_ENV['DATABASE_USERNAME'] . ";password=" . $_ENV['DATABASE_PASSWORD']);
 
     if ($pdo) {
-        echo "Connected to the $db database successfully!";
+        echo "Connected to the database successfully!";
     }
 } catch (PDOException $e) {
     echo $e->getMessage();
