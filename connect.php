09f357d38a4b2e006e80525a12b91ed0b93bb9b1
Loqman Ouagague <loqman.ouagague@gmail.com>
2021-06-12 16:46:38 +0100

    Setting up the evironnement variable

--- connect.php
+++ connect.php
@@ -9,15 +9,7 @@ try {
     $pdo = new PDO($_ENV['DATABASE_DRIVER'] . ":host=" . $_ENV['DATABASE_HOST'] . ";port=" . $_ENV['DATABASE_PORT'] . ";dbname=" . $_ENV['DATABASE_NAME'] . ";user=" . $_ENV['DATABASE_USERNAME'] . ";password=" . $_ENV['DATABASE_PASSWORD']);
 
     if ($pdo) {
-        //$result = $pdo->query("SHOW TABLES LIKE links");
-        //if (count($result) == 0) {
-        $createtable = $pdo->exec("CREATE TABLE links ( id INT NOT NULL AUTO_INCREMENT , redirect_to VARCHAR(10) NOT NULL , redirected_from TEXT NOT NULL , PRIMARY KEY (id));");
-        if ($createtable == true) {
-            echo "done";
-        } else {
-            echo 'no';
-        }
-        //}
+     echo'eee';
     }
 } catch (PDOException $e) {
     echo $e->getMessage();
